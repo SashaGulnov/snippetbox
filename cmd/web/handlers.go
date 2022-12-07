2d57448f5f87417d87cad75ddc6c6b3d33a8b987
SashaGulnov <kirya_kox@hotmail.com>
2022-12-07 16:10:10 +0300

    make db model and db funcs

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"html/template"
 	"net/http"
+	"snippetbox.kirill.ru/internal/models"
 	"strconv"
 )
 
@@ -13,23 +14,34 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	files := []string{
-		"./ui/html/pages/base.tmpl",
-		"./ui/html/pages/nav.tmpl",
-		"./ui/html/pages/home.tmpl",
-	}
-
-	ts, err := template.ParseFiles(files...)
-	if err != nil {
-		app.serverError(w, err)
-		return
-	}
-	err = ts.ExecuteTemplate(w, "base", nil)
+	snippets, err := app.snippets.Latest()
 
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
+
+	for _, snippet := range snippets {
+		fmt.Fprintf(w, "%+v\n\n", snippet)
+	}
+
+	//files := []string{
+	//	"./ui/html/pages/base.tmpl",
+	//	"./ui/html/pages/nav.tmpl",
+	//	"./ui/html/pages/home.tmpl",
+	//}
+	//
+	//ts, err := template.ParseFiles(files...)
+	//if err != nil {
+	//	app.serverError(w, err)
+	//	return
+	//}
+	//err = ts.ExecuteTemplate(w, "base", nil)
+	//
+	//if err != nil {
+	//	app.serverError(w, err)
+	//	return
+	//}
 }
 
 // Add a snippetView handler function.
@@ -39,17 +51,40 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		app.notFound(w)
 		return
 	}
+	snippet, err := app.snippets.Get(id)
+
+	if err != nil {
+		if errors.Is(err, models.ErrNoRows) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+
 	//w.Write([]byte("Display a specific snippet..."))
-	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+	fmt.Fprintf(w, "%+v", snippet)
 }
 
 // Add a snippetCreate handler function.
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
+	if r.Method != http.MethodPost {
 		//w.WriteHeader(405)
 		//w.Write([]byte("Method Not Allowed"))
+		w.Header().Set("Allow", http.MethodPost)
 		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
+	title := "Testing title"
+	content := "Lorem ipsum \ndolor sit amet, consectetur adipisicing elit. \n\nBeatae cumque cupiditate doloribus"
+	expires := 7
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+
 	w.Write([]byte("Create a new snippet..."))
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,11 +7,13 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"snippetbox.kirill.ru/internal/models"
 )
 
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
@@ -37,6 +39,7 @@ func main() {
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	// Use the http.ListenAndServe() function to start a new web server. We pass in
